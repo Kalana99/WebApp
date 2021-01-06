b0e8117fa5e097d1a7bd8dde0824e9c0eb358150
Oshanath <oshanath.19@cse.mrt.ac.lk>
2021-01-07 00:10:51 +0530

    back end 2

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ let requestController = require('./requestHandler.js');
 app.use('/css', express.static(__dirname + '/public/assets/css'));
 app.use('/javascript', express.static(__dirname + '/public/assets/javascript'));
 
-requestController(app);
-app.listen(3000);
+//form data body parser
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
+//Connect to the database
+let mongoose = require('mongoose');
+let dbURI = 'mongodb+srv://oshi:1232@nodetuts.wxb9o.mongodb.net/Nodetuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {app.listen(3000); console.log('You are listening to port 3000');})
+    .catch((err) => console.log(err));
+
+requestController(app);
--- requestHandler.js
+++ requestHandler.js
@@ -1,7 +1,21 @@
-module.exports = function(app) {
+module.exports = function(app){
+    getRequests(app);
+    postRequests(app);
+}
+
+let getRequests = function(app){
 
     app.get('/home', function(req,res){
         res.sendFile(__dirname + '/public/test.html');
     });
 
-};
+    app.get('/', function(req,res){
+        res.redirect('/home');
+    });
+}
+
+let postRequests = function(app){
+
+
+
+}
