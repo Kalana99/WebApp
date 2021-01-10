f70dccceff68027d9c2b90b121b04166c596f0ff
akashTharuka <akash_tharuka@yahoo.com>
2021-01-10 22:18:45 +0530

    nkn

--- public/validation.js
+++ public/validation.js
@@ -1,8 +1,10 @@
 // var check = function() {
 //     if (document.getElementById('psw').value == document.getElementById('c_psw').value) {
-//         document.getElementById('message').style.color = 'green';   
+//         document.getElementById('message').style.color = 'green';
+//         document.getElementById('message').innerHTML = 'matching';   
 //     } 
 //     else {
 //         document.getElementById('message').style.color = 'red';
+//         document.getElementById('message').innerHTML = 'not matching';
 //     }
 //   }
