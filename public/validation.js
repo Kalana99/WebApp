6b18d0e78fb8c1b5607bf425da6e3b61147dd3e3
akashTharuka <akash_tharuka@yahoo.com>
2021-01-10 20:59:36 +0530

    created validation

---
+++ public/validation.js
@@ -0,0 +1,8 @@
+// var check = function() {
+//     if (document.getElementById('psw').value == document.getElementById('c_psw').value) {
+//         document.getElementById('message').style.color = 'green';   
+//     } 
+//     else {
+//         document.getElementById('message').style.color = 'red';
+//     }
+//   }
