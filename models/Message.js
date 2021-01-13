91ca20c1b281495bf49a53f65860df4efe6c564d
Kalana99 <kalana.19@cse.mrt.ac.lk>
2021-01-13 12:44:29 +0530

    models for thread and message

---
+++ models/Message.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Creating schema and model
+const messageSchema = new Schema({
+    "from": String,
+    //"date": String,
+    //"time": String,
+    "text": String,
+    "attachments": {}
+},
+{ timestamps: true });
+
+const Message = mongoose.model('Message', messageSchema);
+module.exports = Message;
---
+++ models/Thread.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Creating schema and model
+const threadSchema = new Schema({
+    "Topic": String,
+    "StaffID": String,
+    "studentID": String,
+    //"date": String,
+    //"time": String,
+    "type": String,
+    "status": String,
+    "messageID_list": [String]
+},
+{ timestamps: true });
+
+const Thread = mongoose.model('Thread', threadSchema);
+module.exports = Thread;
