d4f06f81c30a40c231aece7d757a4d9e97fd2920
akashTharuka <akash_tharuka@yahoo.com>
2021-01-14 01:18:37 +0530

    filled ids

--- public/assets/javascript/validation.js
+++ public/assets/javascript/validation.js
@@ -8,3 +8,38 @@
 //         document.getElementById('message').innerHTML = 'not matching';
 //     }
 //   }
+
+// let validateSubmit = () => {
+//     console.log('success');
+//     let name, index, email, birthday, phone, password, c_password, type;
+//     let gender;
+//     let fac;
+//     if (document.getElementById('male').checked) {
+//         gender = document.getElementById('male').value;
+//     }
+//     else{
+//         gender = document.getElementById('female').value;
+//     }
+
+//     // if (document.getElementById('student').checked) {
+//     //     type = document.getElementById('student').value;
+//     // }
+//     // else{
+//     //     type = document.getElementById('staff').value;
+//     // }
+
+//     // name = document.getElementById('name').value;
+//     // index = document.getElementById('index').value;
+//     // email = document.getElementById('email').value;
+//     // birthday = document.getElementById('birthday').value;
+//     // phone = document.getElementById('phone').value;
+//     // password = document.getElementById('psw').value;
+//     // // c_password = document.getElementById('c_psw').value;
+//     fac = document.getElementById('fac').value;
+//     console.log(fac);
+
+//     // let object = { name, index, email, birthday, phone, password};
+//     // console.log(object);
+
+//     return false;
+// }
