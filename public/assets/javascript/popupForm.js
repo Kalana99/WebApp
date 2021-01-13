02debbedc1f70527258694e85391936521b3a02f
akashTharuka <akash_tharuka@yahoo.com>
2021-01-13 19:36:31 +0530

    edited user profile

--- public/assets/javascript/popupForm.js
+++ public/assets/javascript/popupForm.js
@@ -6,7 +6,6 @@ function openForm(form_num){
     }
     else{
         document.getElementsByClassName("form-popup")[form_num].style.display = "block";
-        document.getElementsByTagName("h2")[0].innerHTML = form_name_array[form_num];
         //haminena aulak thyenwa yata line eke
         document.getElementsByTagName("option")[0].innerHTML = form_name_array[form_num];
         // console.log(document.getElementsByTagName("option")[0].innerHTML = form_name_array[form_num]);
