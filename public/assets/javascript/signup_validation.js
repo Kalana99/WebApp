3646cfa7602ee0772f0bbcf765644824fa537e12
akashTharuka <akash_tharuka@yahoo.com>
2021-01-14 12:30:24 +0530

    signup validation serverside completed

--- public/assets/javascript/signup_validation.js
+++ public/assets/javascript/signup_validation.js
@@ -1,5 +1,16 @@
+// var check = function() {
+//     if (document.getElementById('psw').value == document.getElementById('c_psw').value) {
+//         document.getElementById('message').style.color = 'green';
+//         document.getElementById('message').innerHTML = 'matching';   
+//     } 
+//     else {
+//         document.getElementById('message').style.color = 'red';
+//         document.getElementById('message').innerHTML = 'not matching';
+//     }
+//   }
+
 let form = document.getElementById('form');
-let name = document.getElementById('name');
+let username = document.getElementById('username');
 let index = document.getElementById('index');
 let email = document.getElementById('email');
 let phone = document.getElementById('phone');
@@ -13,27 +24,46 @@ form.addEventListener('submit', (event) => {
 });
 
 function checkInputs(){
+    let isCorrect = true;
     //get the values from the inputs
-    let nameValue = name.value.trim();
+    let userNameValue = username.value.trim();
     let indexValue = index.value.trim();
     let emailValue = email.value.trim();
     let phoneValue = phone.value.trim();
     let passwordValue = password.value.trim();
     let confirmPasswordValue = c_password.value.trim();
 
-    if (nameValue === ''){
+    if (userNameValue === ''){
         //show error
         //add error class
-        setError(name);
+        setError(username, 'Username cannot be blank');
+        isCorrect = false;
     }
     else{
         //add success class
+        // setSuccess(username);
     }
+
+    //last line
+    if (isCorrect){
+        validateSubmit();
+    }
+}
+
+function setError(input, message){
+    let formControl = input.parentElement; // .form-control
+    let small = formControl.querySelector('small');
+
+    //add error message inside small
+    small.innerText = message;
+
+    //add error class
+    formControl.className = 'form-control error';
 }
 
 
 let validateSubmit = function(){
-    let name = document.getElementById('name').value;
+    let name = document.getElementById('username').value;
     let index = document.getElementById('index').value;
     let email = document.getElementById('email').value;
     let birthday = document.getElementById('birthday').value;
