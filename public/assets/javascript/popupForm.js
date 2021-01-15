b34126e1ed5e24547e7f2ab281f28bcde5ee373e
akashTharuka <akash_tharuka@yahoo.com>
2021-01-15 22:42:02 +0530

    logout popup window

--- public/assets/javascript/popupForm.js
+++ public/assets/javascript/popupForm.js
@@ -11,3 +11,18 @@ function openForm(form_num){
         // console.log(document.getElementsByTagName("option")[0].innerHTML = form_name_array[form_num]);
     }
 }
+
+//logout popup window
+let popupWindow = document.getElementById('popup-window');
+
+document.querySelector('#logout-popup').addEventListener('click', () => {
+    popupWindow.className = 'popup-window visible';
+});
+
+document.querySelector('#cancel_logout').addEventListener('click', () => {
+    popupWindow.className = 'popup-window';
+});
+
+document.querySelector('#submit-logout').addEventListener('click', () => {
+    
+});
