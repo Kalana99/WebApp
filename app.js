8179999d3ca3a020748881a1519ef398965ec005
Oshanath <oshanath.19@cse.mrt.ac.lk>
2021-01-16 23:09:45 +0530

    asd

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ mongoose.Promise = global.Promise;
 let local = "mongodb://localhost/StudentRequestSystemDatabase";
 mongoose.connect("mongodb+srv://akash:1234@nodetuts.wxb9o.mongodb.net/StudentRequestSystem?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => {
-        app.listen(3000); 
+        app.listen(process.env.PORT || 3000); 
         console.log('You are listening to port 3000'); 
         requestController(app);
     })
