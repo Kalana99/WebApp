60093e9c26d140c03d1b586160b252a3eead9c0d
Oshanath <oshanath.19@cse.mrt.ac.lk>
2021-01-22 23:13:53 +0530

    asd

--- Controllers/threadController.js
+++ Controllers/threadController.js
@@ -1,3 +1,21 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const Thread = require('../models/Thread');
+const database = require('../database');
+const mail = require('../modules/email');
+
+let mongoose = require('mongoose');
+const db = mongoose.connection;
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb+srv://akash:1234@nodetuts.wxb9o.mongodb.net/StudentRequestSystem?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const maxAge = 1 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({id}, 'esghsierhgoisio43jh5294utjgft*/*/4t*4et490wujt4*/w4t*/t4', {
+        expiresIn: maxAge
+    });
+};
+
 module.exports.getThreadData_get = (req, res) => {
 
     const token = req.cookies.jwt;
