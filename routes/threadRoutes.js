3797aaed9d5710c78df56c6937f0ca027a07bb3b
Oshanath <oshanath.19@cse.mrt.ac.lk>
2021-02-15 11:47:40 +0530

    asd

--- routes/threadRoutes.js
+++ routes/threadRoutes.js
@@ -2,10 +2,12 @@ const {Router} = require('express');
 const router = Router();
 const threadController = require('../Controllers/threadController');
 const {requireAuth} = require('../middleware/authMiddleware');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 router.post('/getThreadData', requireAuth, threadController.getThreadData_post);
 
-router.post('/submitRequest', threadController.submitRequests_post);
+router.post('/submitRequest',upload.single('uploadedFile'), threadController.submitRequests_post);
 
 router.post('/getMessages', threadController.getMessages_post);
 
